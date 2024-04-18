4b67bb9bf53a68c03395d19e648bb7c698a9f755
Ruben3242 <145350644+Ruben3242@users.noreply.github.com>
2024-04-18 11:17:11 +0200

    creaccion del mavenproject

---
+++ Java/procesamiento/src/test/java/com/example/procesamiento/WcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.procesamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
