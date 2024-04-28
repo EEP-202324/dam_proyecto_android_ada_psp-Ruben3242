1a1f73be496d943251cfea12aadbec8076c4b0a2
Ruben3242 <145350644+Ruben3242@users.noreply.github.com>
2024-04-29 01:44:46 +0200

    update de user existente

--- Java/procesamiento/src/main/java/com/example/procesamiento/controller/UserController.java
+++ Java/procesamiento/src/main/java/com/example/procesamiento/controller/UserController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.procesamiento.model.LoginRequest;
@@ -35,10 +35,32 @@ public class UserController {
 		return ResponseEntity.ok(user);
 	}
 
+	@GetMapping("/user")
+	public ResponseEntity<List<User>> getUsers() {
+		List<User> users = userService.getUsers();
+		return ResponseEntity.ok(users);
+	}
+
+	@GetMapping
+	public ResponseEntity<User> getUserByEmail(@PathVariable String email, @PathVariable String password) {
+		User user = userService.getUserByEmail(email, password);
+		return ResponseEntity.ok(user);
+	}
+
 	@PostMapping("/user")
 	public ResponseEntity<User> saveUser(@RequestBody User user) {
 		User savedUser = userService.saveUser(user);
 		return ResponseEntity.ok(savedUser);
+	}
+
+	@PostMapping("/owner/login")
+	public ResponseEntity<?> loginOwner(@RequestBody LoginRequest loginRequest) {
+		boolean isAuthenticated = userService.authenticateUser(loginRequest.getEmail(), loginRequest.getPassword());
+		if (isAuthenticated) {
+			return ResponseEntity.ok().body("Acceso concedido a Owner");
+		} else {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Acceso denegado");
+		}
 	}
 
 	@DeleteMapping("/user/{id}")
@@ -46,35 +68,17 @@ public class UserController {
 		userService.deleteUser(id);
 		return ResponseEntity.ok().build();
 	}
+
 //	@PostMapping("/user")
 //	public ResponseEntity<User> updateUser(@RequestBody User user) {
 //		User updatedUser = userService.updateUser(user);
 //		return ResponseEntity.ok(updatedUser);
 //	} 
-
-	@GetMapping("/user")
-	public ResponseEntity<List<User>> getUsers() {
-		List<User> users = userService.getUsers();
-		return ResponseEntity.ok(users);
+	// updateUser
+	@PutMapping("/user")
+	public ResponseEntity<User> updateUser(@RequestBody User user) {
+		User updatedUser = userService.updateUser(user);
+		return ResponseEntity.ok(updatedUser);
 	}
-	
-	@GetMapping
-	public ResponseEntity<User> getUserByEmail(@PathVariable String email, @PathVariable String password) {
-		User user = userService.getUserByEmail(email, password);
-		return ResponseEntity.ok(user);
-	}
-	
-	@PostMapping("/owner/login")
-	public ResponseEntity<?> loginOwner(@RequestBody LoginRequest loginRequest) {
-	    boolean isAuthenticated = userService.authenticateUser(loginRequest.getEmail(), loginRequest.getPassword());
-	    if (isAuthenticated) {
-	        return ResponseEntity.ok().body("Acceso concedido a Owner");
-	    } else {
-	        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Acceso denegado");
-	    }
-	}
-
-	
-	
 
 }
--- Java/procesamiento/src/test/java/com/example/procesamiento/UserServiceTests.java
+++ Java/procesamiento/src/test/java/com/example/procesamiento/UserServiceTests.java
@@ -1,5 +1,7 @@
 package com.example.procesamiento;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
@@ -7,29 +9,25 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.Assert;
 
 import com.example.procesamiento.model.User;
 import com.example.procesamiento.repository.UserRepository;
 import com.example.procesamiento.service.UserService;
 
-
 @SpringBootTest
 public class UserServiceTests {
 
-    @MockBean
-    private UserRepository userRepository;
+	@MockBean
+	private UserRepository userRepository;
 
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private UserService userService;
 
-    @Test
-    public void testGetUserById() {
-        User user = new User(/*...*/);
-        Mockito.when(userRepository.findById(1)).thenReturn(Optional.of(user));
-        Assert.assertEquals(user, userService.getUser(1));
-    }
+	@Test
+	public void testGetUserById() {
+		User user = new User(/* ... */);
+		Mockito.when(userRepository.findById(1)).thenReturn(Optional.of(user));
+		assertEquals(user, userService.getUser(1));
+	}
 
-    @Test
 }
